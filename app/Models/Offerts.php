06b43afab2886ea58d6711546338e00efa18e640
Kamil Gabrysiewicz <kamilgabrysiewicz60@gmail.com>
2023-10-20 15:57:19 +0200

    Add: OffertController(empty), UserController(done), Models/Offerts(empty)

---
+++ app/Models/Offerts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offerts extends Model
+{
+    use HasFactory;
+}
